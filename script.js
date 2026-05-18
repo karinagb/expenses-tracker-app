e784eaafbea362b2328e84c474d5048f4bb776c0
karinagb <karin.gb27@gmail.com>
2026-05-18 17:01:39 -0600

    Update balance correctly

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ const incomeForm = document.querySelector('.income');
 const expenseForm = document.querySelector('.expense');
 const balanceValue = document.querySelector('.balanceValue');
 const historyItem = document.querySelector('tbody');
-let income = {};
-let expense = {};
-let incomeValue = 0;
-let expenseValue = 0;
-let newBalance = 0;
+let incomeTotal = 0;
+let expenseTotal = 0;
+let balance = 0;
 
 function addIncome(e) {
   e.preventDefault();
-  incomeName = this.querySelector('[name=incomeName]').value;
-  incomeValue = Number(this.querySelector('[name=incomeValue]').value);
+  const incomeName = this.querySelector('[name=incomeName]').value;
+  const incomeValue = Number(this.querySelector('[name=incomeValue]').value);
 
-  income = {
+  const income = {
     description: incomeName,
     value: incomeValue,
   };
+
+  incomeTotal = incomeTotal + incomeValue;
 
   this.reset();
   updateBalance();
@@ -25,13 +25,15 @@ function addIncome(e) {
 
 function addExpense(e) {
   e.preventDefault();
-  expenseName = this.querySelector('[name=expenseName]').value;
-  expenseValue = Number(this.querySelector('[name=expenseValue]').value);
+  const expenseName = this.querySelector('[name=expenseName]').value;
+  const expenseValue = Number(this.querySelector('[name=expenseValue]').value);
 
-  expense = {
+  const expense = {
     description: expenseName,
     value: expenseValue,
   };
+
+  expenseTotal = expenseTotal + expenseValue;
 
   this.reset();
   updateBalance();
@@ -39,13 +41,13 @@ function addExpense(e) {
 }
 
 function updateBalance() {
-  newBalance = incomeValue - expenseValue;
-  return (balanceValue.innerHTML = `<p>$${newBalance}</p>`);
+  balance = incomeTotal - expenseTotal;
+
+  return (balanceValue.innerHTML = `<p>$${balance}</p>`);
 }
 
 function updateHistory(transaction) {
-  return (historyItem.innerHTML += 
-    `<tr>
+  return (historyItem.innerHTML += `<tr>
         <td>${transaction.description}</td>
         <td>$${transaction.value}</td>
     </tr>`);
