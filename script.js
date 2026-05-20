e5808d20fcfdccad8083d79a2c69ba80fa3a1c2d
karinagb <karin.gb27@gmail.com>
2026-05-20 16:38:53 -0600

    Clear items and history & Add income/expense design

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const incomeForm = document.querySelector('.income');
-const expenseForm = document.querySelector('.expense');
+const incomeForm = document.querySelector('.newIncome');
+const expenseForm = document.querySelector('.newExpense');
 const balanceValue = document.querySelector('.balanceValue');
 const historyItems = document.querySelector('tbody');
 const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+const clearButton = document.querySelector('[name=clear]');
 
 let balance = JSON.parse(localStorage.getItem('balance')) || 0;
 let incomeTotal = JSON.parse(localStorage.getItem('incomeTotal')) || 0;
@@ -19,6 +20,7 @@ function addIncome(e) {
   const income = {
     description: incomeName,
     value: incomeValue,
+    type: 'income',
   };
 
   incomeTotal = incomeTotal + incomeValue;
@@ -37,6 +39,7 @@ function addExpense(e) {
   const expense = {
     description: expenseName,
     value: expenseValue,
+    type: 'expense',
   };
 
   expenseTotal = expenseTotal + expenseValue;
@@ -64,15 +67,53 @@ function updateHistory(transaction) {
 }
 
 function renderHistory(transactions) {
-  historyItems.innerHTML = ' ';
+  historyItems.innerHTML = '';
 
-  transactions.forEach((transaction) => {
+  transactions.forEach((transaction, i) => {
     historyItems.innerHTML += `<tr>
         <td>${transaction.description}</td>
-        <td>$${transaction.value}</td>
+        ${transaction.type === 'income' ? 
+            `<td class='income'>+ $${transaction.value}</td>` : 
+            `<td class='expense'>- $${transaction.value}</td>`} 
+        <td><button data-index=${i}>🗑</button></td>
     </tr>`;
   });
 }
 
+function removeTransaction(e) {
+  const index = e.target.dataset.index;
+
+  if (e.target.matches('button[data-index]')) {
+    if (transactions[index].type === 'expense') {
+      expenseTotal = expenseTotal - transactions[index].value;
+      localStorage.setItem('expenseTotal', JSON.stringify(expenseTotal));
+    } else if (transactions[index].type === 'income') {
+      incomeTotal = incomeTotal - transactions[index].value;
+      localStorage.setItem('incomeTotal', JSON.stringify(incomeTotal));
+    }
+    transactions.splice(index, 1);
+    localStorage.setItem('transactions', JSON.stringify(transactions));
+  }
+
+  renderHistory(transactions);
+  updateBalance();
+}
+
+function clearAllData() {
+  incomeTotal = 0;
+  expenseTotal = 0;
+  balance = 0;
+  transactions.length = 0;
+
+  localStorage.removeItem('incomeTotal');
+  localStorage.removeItem('expenseTotal');
+  localStorage.removeItem('balance');
+  localStorage.removeItem('transactions');
+
+  renderBalance(balance);
+  renderHistory(transactions);
+}
 incomeForm.addEventListener('submit', addIncome);
 expenseForm.addEventListener('submit', addExpense);
+clearButton.addEventListener('click', clearAllData);
+document.addEventListener('click', removeTransaction);
